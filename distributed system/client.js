6c1bb43a41e71fe3047a91f36a91c4ad496dae9c
ggdeshmukh12107-droid <ggdeshmukh12107@gmail.com>
2026-02-25 18:34:02 +0530

    Delete distributed system directory

--- distributed system/client.js
+++
@@ -1,9 +0,0 @@
-const net = require("net");
-
-// Connect to the server
-const client = net.createConnection({ port: 5000 }, () => {
-    console.log("Connected to server!");
-
-    // Send message to the server
-    client.write("Hello Server");
-})

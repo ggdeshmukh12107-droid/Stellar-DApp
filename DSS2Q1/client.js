332fa60974171a56c4f71707d1a634db63d89f3c
ggdeshmukh12107-droid <ggdeshmukh12107@gmail.com>
2026-02-25 18:28:52 +0530

    Delete DSS2Q1 directory

--- DSS2Q1/client.js
+++
@@ -1,4 +0,0 @@
-const net = require("net");
-const client = net.createConnection({ port: 5000 }, () => {
-    console.log("Client connected to server");
-});
--- DSS2Q1/server.js
+++
@@ -1,7 +0,0 @@
-const net = require("net");
-const server = net.createServer((socket) => {
-    console.log("Welcome Client"); // printed when client connects
-});
-server.listen(5000, () => {
-    console.log("Server running on port 5000...");
-});
